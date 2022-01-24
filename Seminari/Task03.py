58ca2c30c8fb0c8fa5848479ec7440a6873e91e0
Artur <arturnl@ya.ru>
2022-01-24 17:01:11 +0300

    Task03 add def

--- Seminari/Task03.py
+++ Seminari/Task03.py
@@ -1,8 +1,17 @@
 # 3. По заданному номеру дня недели вывести его название
 
-list = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+listDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 dayWeek = int(input('Введите день недели: '))
 if 0 < dayWeek < 8:
-    print(f'{dayWeek}й день недели - {list[dayWeek-1]}')
+    print(f'{dayWeek}й день недели - {listDays[dayWeek-1]}')
 else:
     print('Такого дня недели не существует')
+
+# Функция
+def NumberDayWeek(a, list):
+    if 0 < a < 8:
+        return list[a-1]
+    else:
+        return 'Такого дня недели не существует'
+print(NumberDayWeek(dayWeek, listDays))
+
