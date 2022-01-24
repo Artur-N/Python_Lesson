c4a811cea281744c68b6bde2d86cd5cd634729d9
Artur <arturnl@ya.ru>
2022-01-24 17:10:07 +0300

    Task04 add def

--- Seminari/Task04.py
+++ Seminari/Task04.py
@@ -1,11 +1,22 @@
 # 4. Найти максимальное из трех чисел
 
-a = 5
-b = 9
-c = 4
+a = 4
+b = 5
+c = 3
 if c <= a >= b:
     print(f'Максимальное число {a}')
 elif c <= b >= a:
     print(f'Максимальное число {b}')
 else:
     print(f'Максимальное число {c}')
+
+# Функция
+
+def Max_of_3(a, b, c):
+    if c <= a >= b:
+        return a
+    elif c <= b >= a:
+        return b
+    else:
+        return c
+print(Max_of_3(a, b, c))
