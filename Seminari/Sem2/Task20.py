3c474a1829880d274918500b980bd1264eff112b
Artur <arturnl@ya.ru>
2022-01-24 22:31:49 +0300

    Task 20

--- Seminari/Sem2/Task20.py
+++ Seminari/Sem2/Task20.py
@@ -2,5 +2,14 @@
 
 a = int(input('Введите номер четверти: '))
 
-
+def NumberQuarter(a):
+    if a == 1:
+        print ('x < 0; y > 0')
+    elif a == 2:
+        print ('x > 0; y > 0')
+    elif a == 3:
+        print ('x > 0; y < 0')
+    elif a == 4:
+        print ('x < 0; y < 0')
+NumberQuarter(a)    
 
