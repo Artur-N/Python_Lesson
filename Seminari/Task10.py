08a442434974938b69714f0074c02356218cb971
Artur <arturnl@ya.ru>
2022-01-24 18:16:57 +0300

    Task10 add def

--- Seminari/Task10.py
+++ Seminari/Task10.py
@@ -1,5 +1,15 @@
 # 10. Показать вторую цифру трёхзначного числа
 
-a = 159
+from random import randint
+
+a = randint(100, 1000)
 b = a // 10 % 10
-print(b)
+print(a)
+print(f'Вторая цифра числа {a} - {b}')
+
+# Функция
+
+def SecondNumber(a):
+    return a // 10 % 10
+print(a)
+print(f'Вторая цифра числа {a} - {SecondNumber(a)}')
