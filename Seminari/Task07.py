5ae17299129f03652c27123ced2119e7d3f2b2b5
Artur <arturnl@ya.ru>
2022-01-24 17:26:02 +0300

    Task07 add def

--- Seminari/Task07.py
+++ Seminari/Task07.py
@@ -1,10 +1,22 @@
 # 7. Показать числа от -N до N
 
-n = -1
+n = -5
 if n >= 0:
     for i in range(-n, n+1):
-        print(i)
+        print(i, end=' ')
 else:
     for i in range(n, -n+1):
-        print(i)
+        print(i, end=' ')
 
+print()
+
+# Функция
+def Show_numbers(N):
+    if N >= 0:
+        for i in range(-N, N+1):
+            print(i, end=' ')
+    else:
+        for i in range(N, -N+1):
+            print(i, end=' ')
+
+Show_numbers(n)
