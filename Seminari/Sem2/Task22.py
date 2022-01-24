27ea2fbbd3e5743ddd92aff34631dfd764a7aff2
Artur <arturnl@ya.ru>
2022-01-24 23:26:13 +0300

    Task 22 Correct

--- Seminari/Sem2/Task22.py
+++ Seminari/Sem2/Task22.py
@@ -1,16 +1,9 @@
 # 22. Найти расстояние между точками в пространстве 2D/3D
 
-x1 = 1
-x2 = 4
-y1 = 1
-y2 = 5
-z1 = 1
-z2 = 3
-
 def Distance(x1, x2, y1, y2):
     return round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5, 2)
-print(Distance(x1, x2, y1, y2))
+print(Distance(1, 4, 1, 5))
 
 def Distance(x1, x2, y1, y2, z1, z2):
     return round(((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2) ** 0.5, 2)
-print(Distance(x1, x2, y1, y2, z1, z2))
+print(Distance(1, 2, 1, 3, 1, 4))
