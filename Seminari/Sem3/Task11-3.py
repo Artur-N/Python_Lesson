f43c8d00190a5553f3e70c7f9e1708364886f99b
Artur <arturnl@ya.ru>
2022-01-29 22:30:10 +0300

    Task11-3

--- Seminari/Sem3/Task11-3.py
+++ Seminari/Sem3/Task11-3.py
@@ -1,4 +1,13 @@
 # Для натурального N создать список: 1, -3, 9, -27, 81 и т.д.
 
+n = 7
 
-
+def ListMulti3(n):
+    list = []
+    for i in range(n):
+        if i % 2 == 0:
+            list.append(3**i)
+        else:
+            list.append(-3**i)
+    return list
+print(ListMulti3(n))
