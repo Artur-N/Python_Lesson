c4e2869918fa896398c56eb75c79b443016d054e
Artur <arturnl@ya.ru>
2022-02-01 17:17:27 +0300

    Task 16

---
+++ Seminari/Sem4/Task16.py
@@ -0,0 +1,15 @@
+# Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму
+
+n = 3
+def Task16(a):
+    list = []
+    k = 0
+    for i in range(1, a+1):
+        r = (1+1/i)**i
+        list.append(round(r, 2))
+        k += r
+    print(list)
+    return round(k, 2)
+print(Task16(n))
+
+
