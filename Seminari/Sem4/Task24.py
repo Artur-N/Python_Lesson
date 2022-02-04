5bd84ca22c4cc69a9931c635e5b691f73583707c
Artur <arturnl@ya.ru>
2022-02-05 01:42:54 +0300

    Task 24

--- Seminari/Sem4/Task24.py
+++ Seminari/Sem4/Task24.py
@@ -1,13 +1,13 @@
 # В заданном списке вещественных чисел найдите разницу между максимальным и минимальным значением дробной части элементов.
 #  Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-list1 = [1.1, 1.2, 3.1, 5, 10.01]
+lst = [1.1, 1.2, 3.1, 5, 10.01]
 
-def Task24(l1):
+def Task24(list):
     list2 = []
-    for i in l1:
+    for i in list:
         if type(i) == float:
             list2.append(round(i - i//1, 2))
     # print(list2)
     return round(max(list2) - min(list2), 2)
-print(Task24(list1))
+print(Task24(lst))
