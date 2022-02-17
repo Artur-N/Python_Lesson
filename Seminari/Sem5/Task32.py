a7e2450c22e0c80bc8d9a9d9f0f517d1352f662b
Artur <arturnl@ya.ru>
2022-02-17 21:51:19 +0300

    Task 32

--- Seminari/Sem5/Task32.py
+++ Seminari/Sem5/Task32.py
@@ -1,16 +1,19 @@
 # Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
 # Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
 
-lst = [10, 2, 3, 5, 1, 5, 3, 10, 1, 2 ,3, 5, 10]
-lst2 = []
-for i in range(len(lst)):
-    a = 0
-    for j in range(len(lst2)):
-        if lst[i] == lst2[j]:
-            a += 1
-    if a == 0:
-        lst2.append(lst[i])
-print(lst2)
+lst = [10, 10, 2, 2, 3, 5, 1, 5, 3, 10, 1, 2 ,3]
+print(lst)
+def Uniq_Seq(list):
+    list2 = []
+    for i in range(len(list)):
+        a = 0
+        for j in range(len(list2)):
+            if list[i] == list2[j]:
+                a += 1
+        if a == 0:
+            list2.append(list[i])
+    return list2
+print(Uniq_Seq(lst))
 
 # 2й вариант
 # def get_unic_value(list):
